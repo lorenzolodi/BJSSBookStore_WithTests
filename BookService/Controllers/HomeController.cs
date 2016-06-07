a7be451300bd8a72564f26a7c138812db32c7cee
drwebb <dan.webb@bjss.com>
2016-06-07 11:53:42 +0100

    Initial commit

---
+++ BookService/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BookService.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BookService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "BJSS Book Store";
+
+            return View();
+        }
+
+        [AuthFilterAttribute]
+        public ActionResult Login(string username, string password)
+        {
+            ViewBag.Title = "BJSS Book Store";
+
+            if (username.ToLower().Replace(" ", "") == "'=''or1=1" && password.ToLower().Replace(" ", "") == "'=''or1=1")
+            {
+                FormsAuthentication.SetAuthCookie("welldone", true);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookService/Filters/AuthFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace BookService.Filters
+{
+    public class AuthFilterAttribute : AuthorizationFilterAttribute
+    {
+
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            //get authentication token from header
+            string authenticationToken = string.Empty;
+            try
+            {
+                if (actionContext.Request.Headers.GetValues("x-user-token") != null && (!string.IsNullOrEmpty(Convert.ToString(actionContext.Request.Headers.GetValues("x-user-token").FirstOrDefault()))))
+                {
+                    authenticationToken = Convert.ToString(actionContext.Request.Headers.GetValues("x-user-token").FirstOrDefault());
+                    if (authenticationToken != null)
+                    {
+                        
+                        try
+                        {
+                            if (authenticationToken == "1234")
+                            {
+                                base.OnAuthorization(actionContext);
+                            }
+                            else
+                            {
+                                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Forbidden);
+                                HttpContext.Current.Response.AddHeader("ErrorMessage", "Forbidden - token incorrect");
+                                return;
+                            }
+                        }
+                        catch
+                        {
+                            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Forbidden);
+                            HttpContext.Current.Response.AddHeader("ErrorMessage", "Forbidden");
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                        HttpContext.Current.Response.AddHeader("ErrorMessage", "Authentication failed");
+                        return;
+                    }
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.PreconditionFailed);
+                    HttpContext.Current.Response.AddHeader("ErrorMessage", "Authentication precondition stage 2 failed");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.PreconditionFailed);
+                HttpContext.Current.Response.AddHeader("ErrorMessage", "Authentication precondition failed - " + ex.Message);
+                return;
+            }
+        }
+    }
+}

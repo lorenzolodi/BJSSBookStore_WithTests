a85995a147d849eef4af6b99c6f3605010895217
Lorenzo Lodi <lorenzo.lodi@bjss.com>
2016-11-07 14:39:06 +0000

    Modified the WebConfig to use the version of local DB

--- BookService/Migrations/Configuration.cs
+++ BookService/Migrations/Configuration.cs
@@ -4,17 +4,28 @@ namespace BookService.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using BookService.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BookService.Models.BookServiceContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(BookService.Models.BookServiceContext context)
         {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
 }
